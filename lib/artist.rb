b0725b6a3c8b1ae834e9ce50de8a75aba0b096ec
Adi Skop <adiskop@gmail.com>
2020-07-21 07:56:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,17 +12,20 @@ class Artist
     @@all
   end
 
-def add_song(song)
+  def add_song(song)
+    song.artist = self
+  end
 
-  song.artist=self
-end
-
-def add_song_by_name(name)
+  def add_song_by_name(name)
     song = Song.new(name)
     song.artist = self
   end
 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 
-
-
+  def self.song_count
+    Song.all.count
+  end
 end
