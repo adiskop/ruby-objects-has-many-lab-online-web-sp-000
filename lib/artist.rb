a0ecfe8da0ffb8db2b1c8b8cdd75f9701d7cd80f
Adi Skop <adiskop@gmail.com>
2020-07-21 07:54:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,15 @@
 class Artist
-attr_accessor :name,
-@songs=[]
-def initialize(name)
-  @name=name
+  attr_accessor :name
 
-end
+  @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-def add_song_by_name(song)
-  @songs<<song
-
-end
-
+  def self.all
+    @@all
+  end
 
 end
